7872fd3e8907d3d287e90f63d8bc58ed1d85a5cc
João Guilherme Rangel <zoroethruz@gmail.com>
2026-04-12 02:56:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def ver_resumo(gastos):
         print("Nenhum gasto")
         return
     
-    total = sum(g["valor"] for g in gastos)
-    print(f"\nTotal de gastos: R$ {total:.2f}")
+    total = sum(gasto["valor"] for gasto in gastos)
+    print(f"Total de gastos: {total:.2f}")
     print(f"Quantidade: {len(gastos)}")
 
 def main():
